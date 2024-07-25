e65abc3d882276525aa19135abff1d3ee2434a1b
malezjaa <66274761+malezjaa@users.noreply.github.com>
2024-07-25 15:35:33 +0200

    Add generated files to ignored

--- ike/src/runtime/js/buffer.mjs
+++
@@ -1,5 +0,0 @@
-const isAscii = $rustFunction("isAscii");
-const atob = $rustFunction("atob");
-const btoa = $rustFunction("btoa");
-const isUtf8 = $rustFunction("isUtf8");
-export { atob, btoa, isAscii, isUtf8 };
